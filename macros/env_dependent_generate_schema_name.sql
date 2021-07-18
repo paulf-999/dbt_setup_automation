8d697f442168df184ef76cbe8330ff005e282020
Paul Fry <paul.fry@versent.com.au>
2021-07-18 18:04:50 +1000

    updated options for 'generate_schema_name' macro

--- macros/env_dependent_generate_schema_name.sql
+++ macros/env_dependent_generate_schema_name.sql
@@ -1,13 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is not none -%}
+    {%- if custom_schema_name is none -%}
 
-        {{ custom_schema_name | trim }}
+        {{ 'beep' }}
 
     {%- else -%}
 
-        {{ default_schema }}
+        {{ 'beep' }}_{{ custom_schema_name | trim }}
 
     {%- endif -%}
 
